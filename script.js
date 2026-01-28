699d8756ac8926e95dca54ff5f07216a8f365cae
Pragati Dixit <164500534+pragatiDxit23@users.noreply.github.com>
2026-01-28 14:20:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function sendMessage(e){
+  e.preventDefault();
+  document.getElementById("popup").style.display="flex";
+}
+
+function closePopup(){
+  document.getElementById("popup").style.display="none";
+}
